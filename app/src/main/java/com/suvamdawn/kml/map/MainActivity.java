707d2a6157d2e28ded8184c110f1af19e4ea1c6a
suvamdawn94 <suvamdawn94@gmail.com>
2019-02-19 17:16:33 +0530

    first commit

---
+++ app/src/main/java/com/suvamdawn/kml/map/MainActivity.java
@@ -0,0 +1,13 @@
+package com.suvamdawn.kml.map;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
